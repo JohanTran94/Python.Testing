568b669ef913760473a4c75c3f3a44a3368422ee
Johan <johan.tranvn@gmail.com>
2024-12-17 11:12:40 +0100

    testing 2

--- Testing1.py
+++ Testing1.py
@@ -1,3 +1,50 @@
+''''
 print ("Hello, World!")
 print("Hello Cuong")
 print("abch")
+'''
+
+'''''
+# Definiera variablerna
+x = 10
+y = 20
+
+# Utför additionen
+z = x + y
+
+# Skriv ut resultatet
+print(f"{x} + {y} = {z}")
+
+
+
+x = 10
+y = 20
+print (f'summan av {x} + {y} är', (x+y))
+
+
+
+x = 10
+y = 20
+summan = x + y
+print (f'summan av {x} + {y} är {summan}')
+'''''
+
+''''
+namn = "Johan"
+ålder = 25
+print (f"Hej, {namn}! Du är {ålder} år gammal.")
+'''
+
+''''
+x = 42
+y = str (x)
+print (y)
+print (type (y))
+'''
+
+x = "123"
+y = 2
+z = int (x)
+resultat = z * y
+print (resultat)
+print (type (resultat))
