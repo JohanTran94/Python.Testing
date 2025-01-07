2a0c4bb5ba8e6f875a5aa7007fb1ac2b16228d8b
Johan <johan.tranvn@gmail.com>
2025-01-07 14:35:12 +0100

    update

--- inlupp/uppgift-4/uppgift_4.py
+++ inlupp/uppgift-4/uppgift_4.py
@@ -1,8 +1,18 @@
 # Uppgift 4
 # Skapa en funktion fibonacci(n) som returnerar en lista med de första n Fibonacci-talen.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def fibonacci(n: int) -> list[int]:
     """
-    Skriv beskrivning här.
+    Returnerar en lista med de första n Fibonacci-talen.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
+    fib_sequence = [0, 1]
+    for x in range(2, n):
+        next_fib = fib_sequence[x - 1] + fib_sequence[x - 2]
+        fib_sequence.append(next_fib)
+    return fib_sequence
+print(fibonacci(10))
