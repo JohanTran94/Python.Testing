28ccd146a6f8f04461a0eb0bc91d936ca7b4b9e3
Johan <johan.tranvn@gmail.com>
2025-01-09 12:12:35 +0100

    update uppgift-7

--- inlupp/uppgift-7/uppgift_7.py
+++ inlupp/uppgift-7/uppgift_7.py
@@ -1,8 +1,15 @@
 # Uppgift 7
 # Skapa en funktion validate_password(password) som kontrollerar att lösenordet är minst 8 tecken långt och innehåller minst en siffra.
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def validate_password(password: str) -> bool:
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    if len(password) >= 8 and any(char.isdigit() for char in password):
+        return True
+    return False
+
+print (validate_password("abc123abr"))
+print (validate_password("ancjj"))
+print (validate_password("anhknfhty"))
+print (validate_password("123654789"))
