4d0a435815578054f4220c2ea5e5cedf0a03c3df
Johan <johan.tranvn@gmail.com>
2025-01-10 11:22:06 +0100

    update uppgift-9

--- inlupp/uppgift-9/uppgift_9.py
+++ inlupp/uppgift-9/uppgift_9.py
@@ -1,8 +1,13 @@
 # Uppgift 9
 # Skapa en funktion is_palindrome(string) som kontrollerar om en given sträng är ett palindrom (dvs. samma framifrån och bakifrån).
 
-def funktions_namn(variabel_namn: datatyp) -> returtyp:
+def is_palindrome(string: str) -> bool:
     """
     Skriv beskrivning här.
     """
-    pass # Ta bort denna rad och skriv din kod här
+    clean_string = string.replace(" ", "").lower()
+    return clean_string == clean_string[::-1]
+
+print(is_palindrome("Radar"))
+print(is_palindrome("hello"))
+print(is_palindrome("A man a plan a canal Panama"))
